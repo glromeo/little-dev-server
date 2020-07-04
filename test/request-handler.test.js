abb94741ee563de23552c76dd3b4a219710b08c4
Gianluca Romeo <glromeo@gmail.com>
2020-07-04 19:10:34 +0100

    Improvements to yarn workspaces management

--- test/request-handler.test.js
+++ test/request-handler.test.js
@@ -8,16 +8,12 @@ describe("Router", function () {
         router = createRouter();
     })
 
-    const h1 = (req, res) => {
-    };
-    const h2 = (req, res) => {
-    };
-    const h3 = (req, res) => {
-    };
-    const h4 = (req, res) => {
-    };
-
     it("can add handlers and route", function () {
+
+        const h1 = jest.fn();
+        const h2 = jest.fn();
+        const h3 = jest.fn();
+        const h4 = jest.fn();
 
         router.get("/abc", h1);
         router.get("/abc/def/ghi", h2);
@@ -53,31 +49,31 @@ describe("Router", function () {
 
         let mock1 = jest.fn();
         router.get("/abc/:name", mock1);
-        router.route(GET, "/abc/def");
+        router.route(GET, "/abc/def").exec();
         expect(mock1).toHaveBeenCalledWith({name: "def"});
 
         let mock2 = jest.fn();
         router.get("/abc/def/:alias", mock2);
-        router.route(GET, "/abc/def/ijk");
+        router.route(GET, "/abc/def/ijk").exec();
         expect(mock2).toHaveBeenCalledWith({alias: "ijk"});
         expect(mock1).toHaveBeenCalledTimes(1);
 
         let mock3 = jest.fn();
         router.get("/abc/xyz", mock3);
-        router.route(GET, "/abc/def");
+        router.route(GET, "/abc/def").exec();
         expect(mock3).not.toHaveBeenCalled();
         expect(mock1).toHaveBeenCalledTimes(2);
 
-        router.route(GET, "/abc/xyz");
+        router.route(GET, "/abc/xyz").exec();
         expect(mock3).toHaveBeenCalledTimes(1);
 
         let mock4 = jest.fn();
         router.get("/abc/:name/:address", mock4);
-        router.route(GET, "/abc/def/ghi");
+        router.route(GET, "/abc/def/ghi").exec();
         expect(mock1).toHaveBeenCalledTimes(2);
         expect(mock2).toHaveBeenCalledTimes(2);
         expect(mock4).not.toHaveBeenCalled();
-        router.route(GET, "/abc/xxx/yyy");
+        router.route(GET, "/abc/xxx/yyy").exec();
         expect(mock4).toHaveBeenCalledWith({name: "xxx", address: "yyy"});
     });
 
@@ -90,9 +86,15 @@ describe("Router", function () {
     it("can handle ** to match the rest of the url", function () {
         const mock = jest.fn();
         router.get("/abc/:name/**", mock);
-        router.route(GET, "/abc/def");
+        router.route(GET, "/abc/def").exec();
         expect(mock).toHaveBeenCalledTimes(0);
-        router.route(GET, "/abc/def/jkl");
+        router.route(GET, "/abc/def/jkl").exec();
         expect(mock).toHaveBeenCalledWith();
+    });
+
+    it("can handle get parameters", function () {
+        const mock = jest.fn();
+        router.get("/abc/:name/**", mock);
+        router.route(GET, "/abc/def").exec();
     });
 });
